a5d94dce6fea1d892827fde6c2b7e28934f79074
Aman <amanclementaranha@gmail.com>
2023-02-25 12:32:36 +0530

    addingbackendurl

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: "http://localhost:5000",
+        target: "instahow.up.railway.app",
         changeOrigin: true,
         secure: false,
         rewrite: path => path.replace('/', ''),
