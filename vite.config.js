fc4078e9f342df4dbb0ec14a23ccaf158b6c1bcf
Aman <amanclementaranha@gmail.com>
2023-02-25 14:41:22 +0530

    maaad

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
+import { createProxyMiddleware } from 'http-proxy-middleware';
 // module.exports = {
 //   server: {
 //     middleware: {
