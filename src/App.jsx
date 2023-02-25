030a8f7a721fcfe6e12960cffc271bf6406eb258
Aman <amanclementaranha@gmail.com>
2023-02-25 13:36:47 +0530

    idk

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const App = ()=>{
   return(
     <div className='App'>
       <Routes>
-      <Route path="/phishy/" element={<Login />} exact />
+      <Route path="/" element={<Login />} exact />
       </Routes>
     </div>
   )
