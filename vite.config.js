846f9bc86196cf7732dc1178799d1c25fae53896
Aman <amanclementaranha@gmail.com>
2023-02-25 14:30:11 +0530

    hmm

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,37 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+// module.exports = {
+//   server: {
+//     middleware: {
+//       // proxy all API requests to the backend server
+//       '/api': createProxyMiddleware({
+//         target: 'https://your-railway-app-url.railway.app',
+//         changeOrigin: true,
+//         pathRewrite: {
+//           '^/api': ''
+//         }
+//       })
+//     }
+//   }
+// };
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base:'/phishy/',
   server: {
-    proxy: {
-      '/api': {
-        target: "instahow.up.railway.app",
+      middleware: {
+      // proxy all API requests to the backend server
+      '/api': createProxyMiddleware({
+        target: 'https://instahow.up.railway.app/',
         changeOrigin: true,
-        secure: false,
-        rewrite: path => path.replace('/', ''),
-      }
+        pathRewrite: {
+          '^/api': ''
+        }
+      })
     }
   },
 }
