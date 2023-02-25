63af83b82e0b673fed12571747496f39beb70a5a
Aman <amanclementaranha@gmail.com>
2023-02-25 14:11:27 +0530

    aa

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
         target: "instahow.up.railway.app",
         changeOrigin: true,
         secure: false,
-        rewrite: path => path.replace('/', ''),
+        rewrite: path => path.reaplace('/', ''),
       }
     }
   },
